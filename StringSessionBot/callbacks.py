97cff8aca1031f55094c2a67f9ee771cc594fcd9
PRAJAPATI PRINCE <79126372+Prince-XD@users.noreply.github.com>
2022-05-26 19:19:55 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -11,42 +11,13 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     # user_id = callback_query.from_user.id
     mention = user["mention"]
     query = callback_query.data.lower()
-    if query.startswith("home"):
-        if query == 'home':
-            chat_id = callback_query.from_user.id
-            message_id = callback_query.message.message_id
-            await bot.edit_message_text(
-                chat_id=chat_id,
-                message_id=message_id,
-                text=Data.START.format(callback_query.from_user.mention, mention),
-                reply_markup=InlineKeyboardMarkup(Data.buttons),
-            )
-    elif query == "about":
-        chat_id = callback_query.from_user.id
-        message_id = callback_query.message.message_id
-        await bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=message_id,
-            text=Data.ABOUT,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(Data.home_buttons),
-        )
-    elif query == "help":
-        chat_id = callback_query.from_user.id
-        message_id = callback_query.message.message_id
-        await bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=message_id,
-            text="**Here's How to use me**\n" + Data.HELP,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(Data.home_buttons),
-        )
-    elif query == "generate":
+    if query.startswith("generate"):
+    if query == "generate":
         await callback_query.message.reply(
             "Please choose the python library you want to generate string session for",
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
-                InlineKeyboardButton("Telethon", callback_data="telethon")
+                InlineKeyboardButton("Pyrogram Music-Bot", callback_data="pyrogram")],
+                [InlineKeyboardButton("Telethon User-Bot", callback_data="telethon")
             ]])
         )
     elif query in ["pyrogram", "telethon"]:
